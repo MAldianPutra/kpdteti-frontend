db9f3d26eb65d2500cb5d34d62da80238a4828b2
Nafisah Hasya S <nafisahhasya@mail.ugm.ac.id>
2021-08-24 12:22:14 +0700

    perubahan PublicationDetailPage.vue

---
+++ src/views/PublicationDetailPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-content>
+    <main-appbar></main-appbar>
+    <v-card></v-card>
+  </v-content>
+</template>
+
+<script>
+import MainAppbar from "@/components/MainAppbar.vue";
+export default {
+  name : "PublicationDetailPage",
+  components :{
+    MainAppbar,
+  },
+}
+</script>
+<style>
+
+</style>
